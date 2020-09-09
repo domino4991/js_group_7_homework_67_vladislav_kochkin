1990f3240be48e18fc359ff5979df168d3b91cc2
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-09-09 14:13:55 +0600

    Добавил обработчики в reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,12 @@
 const initialState = {
     strValue: '',
-    calcResult: null
+    sumOfNums: ''
 };
 
 const reducer = (state = initialState, action) => {
-    if(action.type === 'ADD_VALUE') return {...state, strValue: state.strValue + ' ' + action.value};
+    if(action.type === 'ADD_VALUE') return {...state, strValue: state.strValue + action.value};
+    if(action.type === 'SUM_NUMS') return {...state, sumOfNums: ' = ' + action.value};
+    if(action.type === 'CLEAR') return {...state, strValue: '', sumOfNums: ''};
     return state;
 }
 
